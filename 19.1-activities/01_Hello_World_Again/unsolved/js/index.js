088a5be89d3c8806d757801689b246d4a258f073
aarona65 <68241485+aarona65@users.noreply.github.com>
2021-03-14 11:00:13 -0400

    19.1 activites

--- 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
+++ 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
@@ -1,3 +1,21 @@
 // Create a variable below
 
+
+var helloWorld = "Hello World, I am here";
+console.log(helloWorld);
+
+
+
 // Console.log() your variable here.
+
+
+
+var x = 3;
+var y = 2;
+var z = 'There are '+ (x+y) + ' kids outside';
+
+function addkids(){
+  console.log(z);
+}
+
+addkids();
